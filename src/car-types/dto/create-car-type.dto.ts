c24368d8f685ffbe6b3fcac03e3cd49f1e5cda97
Said Mendoza <ticcitoby777crepis@gmail.com>
2025-02-10 18:24:47 -0500

    feat(car-types): add validation rules to DTO

--- src/car-types/dto/create-car-type.dto.ts
+++ src/car-types/dto/create-car-type.dto.ts
@@ -1,1 +1,9 @@
-export class CreateCarTypeDto {}
+import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
+
+export class CreateCarTypeDto {
+  @IsString()
+  @MaxLength(56, { message: 'The name is too long' })
+  @MinLength(1, { message: 'The name is too short' })
+  @IsNotEmpty()
+  name: string;
+}
