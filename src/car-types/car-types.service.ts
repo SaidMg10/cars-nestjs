0da7b763892e3974079300a263c6cfb266c4244c
Said Mendoza <ticcitoby777crepis@gmail.com>
2025-02-10 20:50:53 -0500

    feat(car-types): add create operation

--- src/car-types/car-types.service.ts
+++ src/car-types/car-types.service.ts
@@ -1,12 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+} from '@nestjs/common';
 import { CreateCarTypeDto } from './dto/create-car-type.dto';
 import { UpdateCarTypeDto } from './dto/update-car-type.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CarType } from './entities/car-type.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class CarTypesService {
-  create(createCarTypeDto: CreateCarTypeDto) {
-    console.log(createCarTypeDto);
-    return 'This action adds a new carType';
+  constructor(
+    @InjectRepository(CarType)
+    private readonly carTypeRepository: Repository<CarType>,
+
+    private readonly logger: Logger,
+  ) {
+    this.logger = new Logger(CarTypesService.name);
+  }
+  async create(createCarTypeDto: CreateCarTypeDto) {
+    const carType = this.carTypeRepository.create(createCarTypeDto);
+    try {
+      return await this.carTypeRepository.save(carType);
+    } catch (error) {
+      this.logger.error(error);
+      throw new InternalServerErrorException(
+        'An internal error while saving the car type',
+      );
+    }
   }
 
   findAll() {
